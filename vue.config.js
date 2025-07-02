efd52d8d35f810ba92b0ba5d05d2805de5b930d0
javihen <javi_hen@hotmail.com>
2025-07-01 23:09:14 -0400

    feat cambios para la publicacion

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/nombre-del-repo/'
+  publicPath: '/pokemon-game/'
 })
